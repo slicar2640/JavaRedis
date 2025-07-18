4eaf908d3dc7073d8d8a0ef9fdcd43e747e0a517
theslinkers2011 <theslinkers2011@gmail.com>
2025-07-18 17:49:52 -0500

    message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -101,57 +101,17 @@ public class Main {
           String getValue = getStored.getValue();
           if (getValue != null) {
             outputWriter.write("$" + getValue.length() + "\r\n" + getValue + "\r\n");
-            outputWriter.flush();
           } else {
             outputWriter.write("$-1\\r\\n");
-            outputWriter.flush();
             storedData.remove(getKey);
             break;
           }
+          outputWriter.flush();
           break;
 
         default:
           break;
       }
-      // while ((content = clientInput.readLine()) != null) {
-
-      // switch (content.toUpperCase()) {
-      // case "PING":
-      // outputWriter.write("+PONG\r\n");
-      // outputWriter.flush();
-      // break;
-      // case "ECHO":
-      // String numBytes = clientInput.readLine();
-      // String message = clientInput.readLine();
-      // outputWriter.write(numBytes + "\r\n" + message + "\r\n");
-      // outputWriter.flush();
-      // break;
-      // case "SET":
-      // clientInput.readLine();
-      // String setKey = clientInput.readLine();
-      // String setValue = clientInput.readLine() + "\r\n" + clientInput.readLine() +
-      // "\r\n";
-      // System.out.println("Set " + setKey + " to " + setValue);
-      // storedData.put(setKey, setValue);
-      // outputWriter.write("+OK\r\n");
-      // outputWriter.flush();
-      // break;
-      // case "GET":
-      // clientInput.readLine();
-      // String getKey = clientInput.readLine();
-      // String getValue = storedData.get(getKey);
-      // if(getValue == null) {
-      // outputWriter.write("$-1\\r\\n");
-      // } else {
-      // outputWriter.write(getValue);
-      // }
-      // outputWriter.flush();
-      // break;
-
-      // default:
-      // break;
-      // }
-      // }
 
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
