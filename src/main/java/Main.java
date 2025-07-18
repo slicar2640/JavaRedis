3b23eee38cd7b716d7bc48b8ababb10e1e4f3997
theslinkers2011 <theslinkers2011@gmail.com>
2025-07-18 14:54:23 -0500

    message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,36 +13,44 @@ public class Main {
     System.out.println("Logs from your program will appear here!");
 
     ServerSocket serverSocket = null;
-    Socket clientSocket = null;
     int port = 6379;
     try {
       serverSocket = new ServerSocket(port);
       // Since the tester restarts your program quite often, setting SO_REUSEADDR
       // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
-      // Wait for connection from client.
-      clientSocket = serverSocket.accept();
 
-      OutputStream outputStream = clientSocket.getOutputStream();
-      InputStream inputStream  = clientSocket.getInputStream();
-
-      while(true) {
-        byte[] input = new byte[1024];
-        inputStream.read(input);
-        System.out.println("Received: " + new String(input).trim());
-        outputStream.write("+PONG\r\n".getBytes());
+      while (true) {
+        Socket clientSocket = serverSocket.accept();
+        System.out.println("New client connected");
+        new Thread(() -> handleClient(clientSocket)).start();
       }
 
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
+    }
+  }
+
+  static void handleClient(Socket clientSocket) {
+    try (clientSocket; // automatically closes socket at the end
+        OutputStream outputStream = clientSocket.getOutputStream();
+        BufferedReader in = new BufferedReader(
+            new InputStreamReader(clientSocket.getInputStream()))) {
+
+      while (true) {
+        if (in.readLine() == null) {
+          break;
         }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
+        in.readLine();
+        String line = in.readLine();
+        System.out.println("Last line: " + line);
+
+        outputStream.write("+PONG\r\n".getBytes());
+        System.out.println("Wrote pong");
       }
+
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
     }
   }
 }
