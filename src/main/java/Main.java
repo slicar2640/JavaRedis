0c752bcd775340a241b22a7ebee63084d93b0a01
theslinkers2011 <theslinkers2011@gmail.com>
2025-07-19 10:34:41 -0500

    Expiry

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -40,80 +39,90 @@ public class Main {
         BufferedWriter outputWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
         InputStream inputStream = clientSocket.getInputStream();) {
       System.out.println("New client connected");
-      String[] line;
-      if ((char) inputStream.read() == '*') {
-        int lengthByte = inputStream.read() - '0';
-        line = new String[lengthByte];
-        inputStream.read();
-        inputStream.read(); // \r\n
-      } else {
-        throw new Exception("Doesn't start with *");
-      }
-
-      for (int i = 0; i < line.length; i++) {
-        char thisChar = (char) inputStream.read();
-        if (thisChar == '$') {
+      while (true) {
+        String[] line;
+        int firstByte = inputStream.read();
+        if (firstByte == -1)
+          break; // client closed connection
+        if ((char) firstByte == '*') {
           int length = 0;
           int digit = 0;
           while ((digit = inputStream.read()) != '\r') {
             length = length * 10 + (digit - '0');
           }
-          System.out.println(length);
           inputStream.read(); // \n
-          byte[] stringBytes = new byte[length];
-          inputStream.read(stringBytes, 0, length);
-          line[i] = new String(stringBytes);
-          inputStream.read();
-          inputStream.read(); // \r\n
+          line = new String[length];
         } else {
-          System.out.println("thisChar: " + thisChar);
-          throw new Exception("Not bulk string, probably should deal with this");
+          throw new Exception("Doesn't start with *");
         }
-      }
+        System.out.println("length: " + line.length);
 
-      String command = line[0];
-      switch (command.toUpperCase()) {
-        case "PING":
-          outputWriter.write("+PONG\r\n");
-          outputWriter.flush();
-          break;
-        case "ECHO":
-          String toEcho = line[1];
-          outputWriter.write("$" + toEcho.length() + "\r\n" + toEcho + "\r\n");
-          outputWriter.flush();
-          break;
-        case "SET":
-          String setKey = line[1];
-          String setValue = line[2];
-          if (line.length == 3) {
-            storedData.put(setKey, setValue);
-          } else {
-            if (line[3].toUpperCase().equals("PX")) {
-              long expiry = Long.valueOf(line[4]);
-              storeWithExpiry(setKey, setValue, expiry);
+        for (int i = 0; i < line.length; i++) {
+          char thisChar = (char) inputStream.read();
+          if (thisChar == '$') {
+            int length = 0;
+            int digit = 0;
+            while ((digit = inputStream.read()) != '\r') {
+              length = length * 10 + (digit - '0');
             }
-          }
-          outputWriter.write("+OK\r\n");
-          outputWriter.flush();
-          break;
-        case "GET":
-          System.out.println("GET");
-          String getKey = line[1];
-          System.out.println("key: " + getKey);
-          String getValue = storedData.get(getKey);
-          if (getValue == null) {
-            outputWriter.write("$-1\r\n");
-            outputWriter.flush();
-            System.out.println("null");
+            System.out.println(length);
+            inputStream.read(); // \n
+            byte[] stringBytes = new byte[length];
+            inputStream.read(stringBytes, 0, length);
+            line[i] = new String(stringBytes);
+            inputStream.read();
+            inputStream.read(); // \r\n
           } else {
-            System.out.println("$" + getValue.length() + "\r\n" + getValue + "\r\n");
-            outputWriter.write("$" + getValue.length() + "\r\n" + getValue + "\r\n");
-            outputWriter.flush();
+            System.out.println("thisChar: " + thisChar);
+            throw new Exception("Not bulk string, probably should deal with this");
           }
-          break;
+        }
 
-        default:
-          break;
+        String command = line[0];
+        System.out.println(command);
+        switch (command.toUpperCase()) {
+          case "PING":
+            outputWriter.write("+PONG\r\n");
+            outputWriter.flush();
+            break;
+          case "ECHO":
+            String toEcho = line[1];
+            outputWriter.write("$" + toEcho.length() + "\r\n" + toEcho + "\r\n");
+            outputWriter.flush();
+            break;
+          case "SET":
+            String setKey = line[1];
+            String setValue = line[2];
+            if (line.length == 3) {
+              storedData.put(setKey, setValue);
+            } else {
+              if (line[3].equalsIgnoreCase("PX")) {
+                long expiry = Long.valueOf(line[4]);
+                storeWithExpiry(setKey, setValue, expiry);
+              }
+            }
+            outputWriter.write("+OK\r\n");
+            outputWriter.flush();
+            break;
+          case "GET":
+            System.out.println("GET");
+            String getKey = line[1];
+            System.out.println("key: " + getKey);
+            String getValue = storedData.get(getKey);
+            if (getValue == null) {
+              outputWriter.write("$-1\r\n");
+              outputWriter.flush();
+              System.out.println("null");
+            } else {
+              System.out.println("$" + getValue.length() + "\r\n" + getValue + "\r\n");
+              outputWriter.write("$" + getValue.length() + "\r\n" + getValue + "\r\n");
+              outputWriter.flush();
+            }
+            break;
+
+          default:
+            break;
+        }
       }
 
     } catch (IOException e) {
@@ -131,7 +140,6 @@ public class Main {
           @Override
           public void run() {
             storedData.remove(key);
-            timer.cancel();
           }
         },
         expiryMillis);
